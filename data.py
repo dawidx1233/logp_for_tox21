a9d0d3eab83d90cb8d890a956f18800de2aae74c
dawidx1233 <85861053+dawidx1233@users.noreply.github.com>
2025-05-27 20:36:02 +0200

    gotowe

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 from rdkit import Chem
 import logging
 import pandas as pd
+from rdkit.Chem import Descriptors
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("MolecularFormulaCalculator")
@@ -21,13 +22,51 @@ class MolecularCalculationTox21:
     def smiles_to_mol(df: pd.DataFrame) -> pd.DataFrame | None:
         try:
             df["mol"] = df["SMILES"].apply(Chem.MolFromSmiles)
+            invalid_smiles = df[df["mol"].isnull()]
+            if not invalid_smiles.empty:
+                logger.error(f"Znaleziono nie prawidłowy SMILES dla: {len(invalid_smiles)} cząsteczek")
+            logger.info("Poprawnie przeknwertowano SMILES na mol")
+            return df
         except Exception as e:
             logger.error(f"Błąd konwersji SMILES: {e}")
             return None
-        invalid_smiles = df[df["mol"].isnull()]
-        if  not invalid_smiles.empty():
-            logger.error(f"Znaleziono nie prawidłowy SMILES dla: {len(invalid_smiles)} cząsteczek")
-        logger.info("Poprawnie przeknwertowano SMILES na mol")
-        return df
 
+    @staticmethod
+    def mol_to_logp(df: pd.DataFrame) -> pd.DataFrame | None:
+        try:
+            df["logp"] = df["mol"].apply(lambda mol: Descriptors.MolLogP(mol) if mol is not None else None)
+            logger.info("Poprawnee przekonwertowanie na logp")
+            return df
+        except Exception as e:
+            logger.error(f"Błąd konwersji logp: {e}")
+            return None
 
+    @staticmethod
+    def average_logp(df: pd.DataFrame) -> float | None:
+        try:
+            average_logp = df["logp"].mean()
+            logger.info("Pomyślnie obliczono średnią")
+            return average_logp
+        except Exception as e:
+            logger.error(f"Błąd podczas obliczania średniej: {e}")
+            return None
+
+    @staticmethod
+    def min_logp(df: pd.DataFrame) -> float | None:
+        try:
+            min_logp = df["logp"].min()
+            logger.info("Pomyślnie znaleziono minimalną wartość")
+            return min_logp
+        except Exception as e:
+            logger.error(f"Błąd podczas szukania minimalnej wartości: {e}")
+            return None
+
+    @staticmethod
+    def max_logp(df: pd.DataFrame) -> float | None:
+        try:
+            max_logp = df["logp"].max()
+            logger.info("Pomyślnie znaleziono maksymalną wartość")
+            return max_logp
+        except Exception as e:
+            logger.error(f"Błąd podczas szukania maksymalną wartości: {e}")
+            return None
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from data import MolecularCalculationTox21 as mct
-
+import os
 
 def main():
     print("Obliczanie statystyk z SMILES lub 'q' aby zakończyć")
     while True:
         input_file = input("Podaj nazwę pliku tox1: ").strip()
+        if not os.path.isfile(input_file):
+            print(f"Plik '{input_file}' nie istnieje.")
+            continue
         if input_file == 'q':
             print("Zakończenie działania programu")
             break
         data = mct.read_smiles_from_file(input_file)
+        if data is None:
+            continue
+        df_mol = mct.smiles_to_mol(data)
+        if df_mol is None:
+            continue
+        df_logp = mct.mol_to_logp(df_mol)
+        if df_logp is None:
+            continue
+        logp_average = mct.average_logp(df_logp)
+        logp_min = mct.min_logp(df_logp)
+        logp_max = mct.max_logp(df_logp)
+        print(f"Dane dla pliku: {input_file}\nŚrednia: {logp_average}\nMin: {logp_min}\nMax: {logp_max}")
+
 
 
 if __name__ == "__main__":
